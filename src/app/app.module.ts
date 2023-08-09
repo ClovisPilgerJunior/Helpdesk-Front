3aeca9ad696834c8b2ecbf7f9dc242f7c8b77de6
Clóvis Júnior <clovispilgerjunior@hotmail.com>
2023-08-09 16:45:10 -0300

    Create technical components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -31,13 +31,15 @@ import { MatCardModule } from '@angular/material/card';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { HeaderComponent } from './components/header/header.component';
+import { TechnicalListComponent } from './components/technical/technical-list/technical-list.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     NavComponent,
     HomeComponent,
-    HeaderComponent
+    HeaderComponent,
+    TechnicalListComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/components/technical/technical-list/technical-list.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-technical-list',
+  templateUrl: './technical-list.component.html',
+  styleUrls: ['./technical-list.component.css']
+})
+export class TechnicalListComponent {
+
+}
