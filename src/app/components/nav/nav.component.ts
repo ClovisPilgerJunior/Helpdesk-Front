320c5033c4c396880a32ea12d2fa92544e15243b
Clóvis Júnior <clovispilgerjunior@hotmail.com>
2023-08-09 14:51:16 -0300

    add home nav route

--- src/app/components/nav/nav.component.ts
+++ src/app/components/nav/nav.component.ts
@@ -1,10 +1,17 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-nav',
   templateUrl: './nav.component.html',
   styleUrls: ['./nav.component.css']
 })
-export class NavComponent {
+export class NavComponent implements OnInit {
+  constructor(private router: Router) {
 
+  }
+
+  ngOnInit(): void {
+    this.router.navigate(['home'])
+  }
 }
