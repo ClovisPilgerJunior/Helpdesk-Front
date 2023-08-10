383fc3e2951efdcb24728eabea12b2f9d7399c64
Clóvis Júnior <clovispilgerjunior@hotmail.com>
2023-08-10 11:41:48 -0300

    Create route to login

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,8 +3,10 @@ import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
 import { TechnicalListComponent } from './components/technical/technical-list/technical-list.component';
+import { LoginComponent } from './components/login/login.component';
 
 const routes: Routes = [
+  { path: 'login', component: LoginComponent},
   {
     path: '', component: NavComponent, children: [
       { path: 'home', component: HomeComponent },
