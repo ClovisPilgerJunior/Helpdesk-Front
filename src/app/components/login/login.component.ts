8c6416047a70660d27fc91585e5f00fe9697386d
Clóvis Júnior <clovispilgerjunior@hotmail.com>
2023-08-10 16:48:02 -0300

    Update login components

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
+import { Credentials } from 'src/app/models/credentials';
 
 @Component({
   selector: 'app-login',
@@ -6,5 +8,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./login.component.css']
 })
 export class LoginComponent {
+
+  creds: Credentials = {
+    email: '',
+    password: ''
+  }
+
+  email = new FormControl(null, Validators.email);
+  password = new FormControl(null, Validators.minLength(3));
 
 }
