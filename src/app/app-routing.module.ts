2d3e5b1007b9b4e7089ea2592b4f86bb6f5a5e47
Clóvis Júnior <clovispilgerjunior@hotmail.com>
2023-08-10 10:55:10 -0300

    Update app-routing for technical

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,11 +2,13 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NavComponent } from './components/nav/nav.component';
 import { HomeComponent } from './components/home/home.component';
+import { TechnicalListComponent } from './components/technical/technical-list/technical-list.component';
 
 const routes: Routes = [
   {
     path: '', component: NavComponent, children: [
-      { path: 'home', component: HomeComponent }
+      { path: 'home', component: HomeComponent },
+      { path: 'technical', component: TechnicalListComponent }
     ]
   }
 ];
