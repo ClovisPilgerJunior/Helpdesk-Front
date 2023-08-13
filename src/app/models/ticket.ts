5acce18fd1d90a7c7a78ebed7bc05b9fb5a06684
Clovis Pilger Junior <clovispilgerjunior@hotmail.com>
2023-08-13 08:11:49 -0300

    Create ticket interface

---
+++ src/app/models/ticket.ts
@@ -0,0 +1,13 @@
+export interface Ticket {
+    id?: any;                        
+    openingDate?: string;    
+    closingDate?: string;    
+    priority: string;       
+    status: string;         
+    title: string;          
+    observations: string;   
+    technical: any;      
+    customer: any;       
+    technicianName: string; 
+    customerName: string;   
+}
