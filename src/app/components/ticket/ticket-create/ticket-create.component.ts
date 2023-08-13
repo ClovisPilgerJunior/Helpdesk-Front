e259aceceb4e06878f5e8349d494521f0fc1ef2a
Clovis Pilger Junior <clovispilgerjunior@hotmail.com>
2023-08-13 11:22:32 -0300

    Create form validators

--- src/app/components/ticket/ticket-create/ticket-create.component.ts
+++ src/app/components/ticket/ticket-create/ticket-create.component.ts
@@ -1,10 +1,34 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FormControl, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-ticket-create',
   templateUrl: './ticket-create.component.html',
   styleUrls: ['./ticket-create.component.css']
 })
-export class TicketCreateComponent {
+export class TicketCreateComponent implements OnInit {
 
+  title: FormControl = new FormControl(null, [Validators.required])
+  status: FormControl = new FormControl(null, [Validators.required])
+  priority: FormControl = new FormControl(null, [Validators.required])
+  technicianName: FormControl = new FormControl(null, [Validators.required])
+  customerName: FormControl = new FormControl(null, [Validators.required])
+  observations: FormControl = new FormControl(null, [Validators.required])
+
+  constructor() {
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  fieldValidate(): boolean {
+    return this.title.valid &&
+          this.status.valid &&
+          this.priority.valid &&
+          this.technicianName &&
+          this.customerName.valid &&
+          this.observations.valid
+  }
 }
